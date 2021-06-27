6bc08421b1be7afc81b71779d6c5180878eb4909
Alvaro Alejandro Arancibia <aljand321@gmail.com>
2021-06-26 21:57:35 -0400

    solo commit

--- src/components/pacientes/Forms/FormConsulta.js
+++ src/components/pacientes/Forms/FormConsulta.js
@@ -1,57 +1,114 @@
 /* import {useForm} from 'react-hook-form'; */
+import React, { useState } from 'react';
+const form = {
+    motivo:'',
+    enfermedadActual:'',
+    enfermedadActual:{
+        ta:'',
+        fc:'',
+        fr:'',
+        sao:'',
+        temp:{
+            r:null,
+            data:''
+        }
+    }
+}
+ 
 function FormConsutla(props) {
     /* const { register, handleSubmit, reset, formState: { errors } } = useForm(); */
+    const [data, setData] = useState(form);
+    const [load, setLoad] = useState(false);
+    const [err, setErr] = useState(false);
+    const [resp, setResp] = useState(false);
+    
+    const handleChange = (e) => {
+        const {value, name} = e.target;
+        console.log('value',value,' ','name',name );
+    }
+
     return (
         <>
             <form>
                 <div className="card-body">
                     <div className="form-group">
                         <label htmlFor="exampleInputEmail1">Motivo de Consulta</label>
-                        <textarea className="form-control" placeholder='Motivo de consulta' rows="5"></textarea>
+                        <textarea 
+                        name="motivo" 
+                        onChange={handleChange}
+                        className="form-control" 
+                        placeholder='Motivo de consulta' 
+                        rows="5"
+                        value={data.motivo}
+                        ></textarea>
                     </div>
                     <div className="form-group">
                         <label htmlFor="exampleInputPassword1">Historia de la enfermedad actual</label>
-                        <textarea className="form-control"  rows="5"></textarea>
+                        <textarea 
+                        name="enfermedadActual"
+                        onChange={handleChange}
+                        className="form-control"  
+                        rows="5"
+                        value={data.enfermedadActual}
+                        ></textarea>
                     </div>
-                    <div className="form-group">                       
-                
-                        <div className="input-group">
+
+                    <div className="row">
+                        <div className="col-md-6">
                             <div className="form-group">
                                 <label htmlFor="exampleInputEmail2">T.A. mmHg / Glasgow</label>                                
-                                <textarea className="form-control"  rows="4" placeholder=" mmHg / Glasgow"></textarea>
+                                <textarea 
+                                name="ta" 
+                                onChange={handleChange}
+                                className="form-control"  
+                                rows="2" 
+                                value={data.enfermedadActual.ta}
+                                placeholder=" mmHg / Glasgow"></textarea>
                             </div>                            
                             <div className="form-group">
-                                <label htmlFor="exampleInputEmail1">F.C. lat/min / 15 Glicemia</label>
-                                <textarea className="form-control"  rows="4" placeholder="F.C. lat/min / 15 Glicemia"></textarea>
-                            </div>
-                            <div className="form-group">
                                 <label htmlFor="exampleInputEmail1">F.R. C/min mg% Peso</label>
-                                <textarea className="form-control"  rows="4" placeholder="F.R. C/min mg% Peso"></textarea>
+                                <textarea 
+                                name="fr"
+                                onChange={handleChange}
+                                className="form-control"  
+                                rows="2" 
+                                value={data.enfermedadActual.fr}
+                                placeholder="F.R. C/min mg% Peso"></textarea>
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="exampleInputEmail1">Temp. °C Aux. Rec. cm  IMC</label>          
+                                <div className="form-group clearfix">
+                                    <div className="icheck-primary d-inline">
+                                        <input type="radio" id="radioPrimary1" name="r1" defaultChecked />
+                                            <label htmlFor="radioPrimary1">
+                                                Aux.
+                                            </label>
+                                    </div>
+                                    <div className="icheck-primary d-inline">
+                                        <input type="radio" id="radioPrimary2" name="r1" />
+                                            <label htmlFor="radioPrimary2">
+                                                Rec.
+                                            </label>
+                                    </div>                                    
+                                </div>
+                                                     
+                                <textarea className="form-control" rows="2" placeholder="Temp. °C Aux. Rec. cm  IMC"></textarea>
+                            </div>
+
+                        </div>
+                        <div class="col-md-6">
+                            <div className="form-group">
+                                <label htmlFor="exampleInputEmail1">F.C. lat/min / 15 Glicemia</label>
+                                <textarea className="form-control"  rows="2" placeholder="F.C. lat/min / 15 Glicemia"></textarea>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="exampleInputEmail1">SaO2 % Kg Talla</label>
-                                <textarea className="form-control"  rows="4" placeholder="SaO2 % Kg Talla"></textarea>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="exampleInputEmail1">Temp. °C Aux. Rec. cm  IMC</label>
-                                
-                                <textarea className="form-control" rows="4" placeholder="Temp. °C Aux. Rec. cm  IMC"></textarea>
-                                <div className="form-check">
-                                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                                    <label className="form-check-label" htmlFor="exampleCheck1">Aux.</label>
-                                    
-                                </div>
-                                <div className="form-check">
-                                    <input type="checkbox" className="form-check-input" id="exampleCheck2" />
-                                    <label className="form-check-label" htmlFor="exampleCheck2">Rec.</label>
-                                    
-                                </div>
-
+                                <textarea className="form-control"  rows="2" placeholder="SaO2 % Kg Talla"></textarea>
                             </div>
                             
                         </div>
-                    </div>
-                    
+                    </div>                    
                 </div>
                 {/* /.card-body */}
                 <div className="d-flex">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,72 @@
-import React from 'react';
-
-class Home extends React.Component{
-    render() {
-        return(
-            <React.Fragment>
-                <h1>esto es el home</h1>
-            </React.Fragment>
-        );
+import React from "react";
+import ContHeader from "../components/ContHeader";
+import ListasMedicos from "../components/Home/listasMedicos";
+//import { Link } from 'react-router-dom';
+class Home extends React.Component {
+    state={
+        esto:{
+            name:'martin',
+            lastName:"flores"
+        }
     }
+    algo = (data) =>{
+        console.log(data, ' sdasdasd')
+    }
+  render() {
+    return (
+      <React.Fragment>
+        <section className="content">
+          <div className="container-fluid">
+            <ContHeader name="Informacion">
+              <h1>----</h1>
+            </ContHeader>
+            <div class="row">
+              <div className="col-lg-3 col-6">
+                {/* small box */}
+                <div className="small-box bg-info">
+                  <div className="inner">
+                    <h3>150</h3>
+                    <p>New Orders</p>
+                  </div>
+                  <div className="icon">
+                    <i className="ion ion-bag" />
+                  </div>
+                  <a href="#" className="small-box-footer">
+                    More info <i className="fas fa-arrow-circle-right" />
+                  </a>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-6">
+                {/* small box */}
+                <div className="small-box bg-info">
+                  <div className="inner">
+                    <h3>150</h3>
+                    <p>New Orders</p>
+                  </div>
+                  <div className="icon">
+                    <i className="ion ion-bag" />
+                  </div>
+                  <a href="#" className="small-box-footer">
+                    More info <i className="fas fa-arrow-circle-right" />
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <section className="col-lg-7 connectedSortable">
+                {/* TO DO List */}
+                <ListasMedicos tiutlo='docotres' click={this.algo} datas={this.state.esto}>
+                    <h1>hola aljand</h1>
+                </ListasMedicos>
+              </section>
+            </div>
+
+          </div>
+        </section>
+      </React.Fragment>
+    );
+  }
 }
 export default Home;
