bdc08a81cee63373f477f1fdfa26cb2bea5c4d8e
wenv <lmiriam521@gmail.com>
2021-07-24 22:14:42 -0400

    Grafica2

--- src/components/Dashboard/Graphics.js
+++ src/components/Dashboard/Graphics.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BorderColor } from '@material-ui/icons';
 import {Line} from 'react-chartjs-2';
-import 'Graphics.css'
+import './css/Graphics.css';
 
  function Graphics(props) {
     const data={
